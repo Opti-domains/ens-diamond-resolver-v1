33c824658b7bb32e09f956b66ddd78b4a5cf74fb
Chomtana <Chomtana001@gmail.com>
2023-05-24 19:20:28 +0700

    OptiDomainsSocialOracle success

--- test/resolvers/TestPublicResolverExtended.js
+++ test/resolvers/TestPublicResolverExtended.js
@@ -6,6 +6,7 @@ const OptiDomainsAttestation = artifacts.require('OptiDomainsAttestation.sol')
 const NameWrapperRegistry = artifacts.require('NameWrapperRegistry.sol')
 const NameWrapper = artifacts.require('MockNameWrapper.sol')
 const RegistryWhitelistAuthFacet = artifacts.require('RegistryWhitelistAuthFacet.sol')
+const OptiDomainsSocialOracle = artifacts.require('OptiDomainsSocialOracle.sol')
 const PublicResolverFacet = artifacts.require('PublicResolverFacet.sol')
 const TestAddrResolver = artifacts.require('TestAddrResolver.sol')
 const TestWeirdResolver = artifacts.require('TestWeirdResolver.sol')
@@ -85,75 +86,6 @@ async function deployPublicResolverFacet(_diamondResolver) {
     ethers.utils.id("setABI(bytes32,uint256,bytes)").substring(0, 10),
     ethers.utils.id("setAddr(bytes32,uint256,bytes)").substring(0, 10),
     ethers.utils.id("setAddr(bytes32,address)").substring(0, 10),
-    ethers.utils.id("setContenthash(bytes32,bytes)").substring(0, 10),
-    ethers.utils.id("setDNSRecords(bytes32,bytes)").substring(0, 10),
-    ethers.utils.id("setInterface(bytes32,bytes4,address)").substring(0, 10),
-    ethers.utils.id("setName(bytes32,string)").substring(0, 10),
-    ethers.utils.id("setPubkey(bytes32,bytes32,bytes32)").substring(0, 10),
-    ethers.utils.id("setText(bytes32,string,string)").substring(0, 10),
-    ethers.utils.id("setZonehash(bytes32,bytes)").substring(0, 10),
-    ethers.utils.id("text(bytes32,string)").substring(0, 10),
-    ethers.utils.id("zonehash(bytes32)").substring(0, 10),
-  ]
-
-  const facetCut = {
-    target: publicResolver.address,
-    action: 0, // ADD
-    selectors: selectors
-  }
-
-  const supportInterfaces = [
-    "0x2203ab56", // IABIResolver
-    "0xf1cb7e06", // IAddressResolver
-    "0x3b3b57de", // IAddrResolver
-    "0xbc1c58d1", // IContentHashResolver
-    "0xa8fa5682", // IDNSRecordResolver
-    "0x5c98042b", // IDNSZoneResolver
-    "0x124a319c", // IInterfaceResolver
-    "0x691f3431", // INameResolver
-    "0xc8690233", // IPubKeyResolver
-    "0x59d1d43c", // ITextResolver
-  ]
-
-  const tx1 = await diamondResolver.diamondCut(
-    [facetCut],
-    // "0x0000000000000000000000000000000000000000",
-    diamondResolver.address, 
-    // "0x",
-    diamondResolver.interface.encodeFunctionData(
-      "setMultiSupportsInterface",
-      [
-        supportInterfaces,
-        true,
-      ]
-    ),
-  )
-
-  await tx1.wait()
-
-  return await PublicResolverFacet.at(diamondResolver.address)
-}
-
-async function deployPublicResolverFacet(_diamondResolver) {
-  const diamondResolver = await (
-    await ethers.getContractFactory('DiamondResolver')
-  ).attach(_diamondResolver.address)
-
-  const publicResolver = await PublicResolverFacet.new();
-
-  const selectors = [
-    ethers.utils.id("ABI(bytes32,uint256)").substring(0, 10),
-    ethers.utils.id("addr(bytes32)").substring(0, 10),
-    ethers.utils.id("addr(bytes32,uint256)").substring(0, 10),
-    ethers.utils.id("contenthash(bytes32)").substring(0, 10),
-    ethers.utils.id("dnsRecord(bytes32,bytes32,uint16)").substring(0, 10),
-    ethers.utils.id("hasDNSRecords(bytes32,bytes32)").substring(0, 10),
-    ethers.utils.id("interfaceImplementer(bytes32,bytes4)").substring(0, 10),
-    ethers.utils.id("name(bytes32)").substring(0, 10),
-    ethers.utils.id("pubkey(bytes32)").substring(0, 10),
-    ethers.utils.id("setABI(bytes32,uint256,bytes)").substring(0, 10),
-    ethers.utils.id("setAddr(bytes32,uint256,bytes)").substring(0, 10),
-    ethers.utils.id("setAddr(bytes32,address)").substring(0, 10),
     ethers.utils.id("setAddrWithRef(bytes32,uint256,bytes32,bytes)").substring(0, 10),
     ethers.utils.id("setContenthash(bytes32,bytes)").substring(0, 10),
     ethers.utils.id("setDNSRecords(bytes32,bytes)").substring(0, 10),
@@ -203,6 +135,35 @@ async function deployPublicResolverFacet(_diamondResolver) {
   await tx1.wait()
 
   return await PublicResolverFacet.at(diamondResolver.address)
+}
+
+async function deploySocialOracle(_diamondResolver, operator, attestation) {
+  const diamondResolver = await (
+    await ethers.getContractFactory('DiamondResolver')
+  ).attach(_diamondResolver.address)
+
+  const oracle = await OptiDomainsSocialOracle.new(operator, attestation);
+
+  const selectors = [
+    ethers.utils.id("setWalletWithVerification(bytes32,address,uint256,bytes,bytes,bytes)").substring(0, 10),
+    ethers.utils.id("setSocialProfile(bytes32,address,string,string,bytes,bytes)").substring(0, 10),
+  ]
+
+  const facetCut = {
+    target: oracle.address,
+    action: 0, // ADD
+    selectors: selectors
+  }
+
+  const tx1 = await diamondResolver.diamondCut(
+    [facetCut],
+    "0x0000000000000000000000000000000000000000",
+    "0x",
+  )
+
+  await tx1.wait()
+
+  return [await OptiDomainsSocialOracle.at(diamondResolver.address), await OptiDomainsSocialOracle.at(oracle.address)]
 }
 
 async function deployTestAddrResolver(_diamondResolver, action = 0) {
@@ -359,15 +320,110 @@ async function registerSchema(schemaRegistry) {
   await schemaRegistry.register("bytes32 node,string name", "0x0000000000000000000000000000000000000000", true);
   await schemaRegistry.register("bytes32 node,string key,string value", "0x0000000000000000000000000000000000000000", true);
   await schemaRegistry.register("bytes32 node,bytes32 x,bytes32 y", "0x0000000000000000000000000000000000000000", true);
+
+  await schemaRegistry.register("bytes32 node,uint256 coinType,bytes identity,bytes proof", "0x0000000000000000000000000000000000000000", true);
+  await schemaRegistry.register("bytes32 node,string provider,string identity,bytes proof", "0x0000000000000000000000000000000000000000", true);
 }
 
 function generateSalt(address) {
   return address + crypto.randomBytes(12).toString('hex')
 }
 
+function generateSocialOracleSignature(address, schema, data) {
+  // Define the input types and values of the transaction data
+  const inputTypes = [
+    'bytes1',
+    'bytes1',
+    'address',
+    'uint256',
+    'bytes32',
+    'bytes32',
+    'bytes32',
+  ]
+  const inputValues = [
+    '0x19',
+    '0x00',
+    address,
+    network.config.chainId,
+    '0x377446750a403c1b4014436073cf8d08ceadc5b156ac1c8b7b0ca41a0c9c1c54',
+    schema,
+    ethers.utils.keccak256(data),
+  ]
+
+  // ABI-encode the transaction data
+  const digest = ethers.utils.solidityKeccak256(inputTypes, inputValues)
+
+  // console.log(
+  //   digest,
+  //   controller.address,
+  //   network.config.chainId,
+  //   isTakeover
+  //     ? '0x0548274c4be004976424de9f6f485fbe40a8f13e41524cd574fead54e448415c'
+  //     : '0xdd007bd789f73e08c2714644c55b11c7d202931d717def434e3c9caa12a9f583',
+  //   commitment,
+  // )
+
+  const signingKey = new ethers.utils.SigningKey(process.env.DEPLOYER_KEY)
+  const signature = signingKey.signDigest(digest)
+
+  return ethers.utils.hexlify(
+    ethers.utils.concat([
+      signature.r,
+      signature.s,
+      ethers.utils.hexlify(signature.v),
+    ]),
+  )
+}
+
+function generateSocialOracleRevokeSignature(address, schema, uid) {
+  // Define the input types and values of the transaction data
+  const inputTypes = [
+    'bytes1',
+    'bytes1',
+    'address',
+    'uint256',
+    'bytes32',
+    'bytes32',
+    'bytes32',
+  ]
+  const inputValues = [
+    '0x19',
+    '0x00',
+    address,
+    network.config.chainId,
+    '0x9e10ea5887e56efeb96d4464ee7be8e8f408f1a889563a625d293d9d970cc73f',
+    schema,
+    uid,
+  ]
+
+  // ABI-encode the transaction data
+  const digest = ethers.utils.solidityKeccak256(inputTypes, inputValues)
+
+  // console.log(
+  //   digest,
+  //   controller.address,
+  //   network.config.chainId,
+  //   isTakeover
+  //     ? '0x0548274c4be004976424de9f6f485fbe40a8f13e41524cd574fead54e448415c'
+  //     : '0xdd007bd789f73e08c2714644c55b11c7d202931d717def434e3c9caa12a9f583',
+  //   commitment,
+  // )
+
+  const signingKey = new ethers.utils.SigningKey(process.env.DEPLOYER_KEY)
+  const signature = signingKey.signDigest(digest)
+
+  return ethers.utils.hexlify(
+    ethers.utils.concat([
+      signature.r,
+      signature.s,
+      ethers.utils.hexlify(signature.v),
+    ]),
+  )
+}
+
 contract('PublicResolver', function (accounts) {
   let node
-  let ens, resolver, nameWrapper, auth, diamondResolver, nameWrapperRegistry, attestation, schemaRegistry, eas
+  let ens, resolver, nameWrapper, auth, diamondResolver, nameWrapperRegistry, attestation, schemaRegistry, eas, oracleResolver, oracle
   let account
   let signers
   let result
@@ -400,6 +456,9 @@ contract('PublicResolver', function (accounts) {
 
     auth = await deployWhitelistAuthFacet(diamondResolver)
     resolver = await deployPublicResolverFacet(diamondResolver)
+    const oracleDeployment = await deploySocialOracle(diamondResolver, new ethers.Wallet(process.env.DEPLOYER_KEY).address, attestation.address)
+    oracleResolver = oracleDeployment[0]
+    oracle = oracleDeployment[1]
 
     // resolver = new web3.eth.Contract(PublicResolverABI, diamondResolver.address)
 
@@ -439,6 +498,32 @@ contract('PublicResolver', function (accounts) {
 
     expect(textRefAtt.uid).to.equal(addrAtt.uid);
     expect(textRefAtt.refUID).to.equal(addrAtt0.uid);
+  })
+
+  it('Social Oracle: Wallet address', async () => {
+    const WALLET_ORACLE_SCHEMA = ethers.utils.solidityKeccak256(['string', 'address', 'bool'], ["bytes32 node,uint256 coinType,bytes identity,bytes proof", "0x0000000000000000000000000000000000000000", true])
+    const SOCIAL_ORACLE_SCHEMA = ethers.utils.solidityKeccak256(['string', 'address', 'bool'], ["bytes32 node,string provider,string identity,bytes proof", "0x0000000000000000000000000000000000000000", true])
+
+    const ADDRESS_SCHEMA = ethers.utils.solidityKeccak256(['string', 'address', 'bool'], ["bytes32 node,uint256 coinType,bytes address", "0x0000000000000000000000000000000000000000", true])
+    const TEXT_SCHEMA = ethers.utils.solidityKeccak256(['string', 'address', 'bool'], ["bytes32 node,string key,string value", "0x0000000000000000000000000000000000000000", true])
+
+    await oracleResolver.setWalletWithVerification(
+      node,
+      oracle.address,
+      60,
+      accounts[1],
+      '0x1234',
+      generateSocialOracleSignature(
+        oracle.address,
+        WALLET_ORACLE_SCHEMA,
+        ethers.utils.defaultAbiCoder.encode(
+          ['bytes32', 'uint256', 'bytes', 'bytes'],
+          [node, 60, accounts[1], '0x1234']
+        )
+      )
+    )
+
+    assert.equal(await resolver.methods['addr(bytes32)'](node), accounts[1])
   })
 
   it('Can clone DiamondResolver', async () => {
